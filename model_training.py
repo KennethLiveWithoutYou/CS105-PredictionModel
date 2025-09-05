1ea0e6b060576d2b910f2a234c3d801424def7ce
Utarou22 <lkennethjan@gmail.com>
2025-09-05 15:35:09 +0800

    Initial commit: Flask app with ML training scripts

---
+++ model_training.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import os
+
